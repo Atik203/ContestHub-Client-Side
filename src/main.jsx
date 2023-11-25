d02c6cb6c5162dc889d5a01ba052ab08b9711627
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-11-25 19:31:56 +0600

    initialized the project. install firebase,tailwind,daisy ui and others package

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import Root from "./Root/Root";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root></Root>
+  </React.StrictMode>
+);
