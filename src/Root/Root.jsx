22b86b1f58219f844ad2f214185d8228827207c8
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-11-25 19:51:09 +0600

    created main layout and components,routes

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "./../Components/Footer/Footer";
 
 const Root = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bg-base-100 flex min-h-screen flex-col">
+      <div className="flex-grow">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./../Components/Login/Login";
+import Register from "./../Components/Login/Register/Register";
+import Home from "./../Pages/Home/Home";
+import ErrorPage from "./../Components/ErrorPage/ErrorPage";
+import Root from "../Root/Root";
 
-const Routes = () => {
-  return <div></div>;
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
 
-export default Routes;
+export default router;
